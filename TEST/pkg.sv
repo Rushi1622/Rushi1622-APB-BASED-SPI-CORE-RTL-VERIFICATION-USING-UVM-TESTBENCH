e91eaac7ff53258162f43bfb777c6655375df88a
Rushikesh Balasaheb Wakde <155816684+Rushi1622@users.noreply.github.com>
2026-05-30 16:42:24 +0530

    Update pkg.sv

--- TEST/pkg.sv
+++ TEST/pkg.sv
@@ -1,1 +1,67 @@
+package apb_spi_pkg;
 
+	import uvm_pkg::*;
+	`include "uvm_macros.svh" 
+	
+  // Config for spi and apb
+	
+	
+ 
+  `include "spi_config.sv"
+  `include "apb_config.sv"
+    `include "env_config.sv"  ///add here env first these both 
+
+  // Transaction
+  `include "spi_xtn.sv"
+  `include "apb_xtn.sv"
+
+  
+  // SEQUENCE RELATED
+  
+
+  `include "spi_sequencer.sv"
+  `include "apb_sequencer.sv"
+
+  `include "spi_sequence.sv"
+  
+  `include "apb_sequence.sv"
+
+  
+  // COMPONENTS
+  
+
+  `include "spi_driver.sv"
+  `include "spi_monitor.sv"
+ 
+  `include "apb_driver.sv"
+  `include "apb_monitor.sv"
+
+
+  //------------------------------------------
+  // AGENT
+  //------------------------------------------
+
+  `include "spi_agent.sv"
+  `include "apb_agent.sv"
+
+	
+  `include "spi_agent_top.sv"	
+  `include "apb_agent_top.sv"
+
+
+  //------------------------------------------
+  // ENV + SCOREBOARD
+  //------------------------------------------
+
+  `include "scoreboard.sv"
+  
+  `include "env.sv"
+
+  //------------------------------------------
+  // TEST LAST
+  //------------------------------------------
+
+  `include "test.sv"
+
+
+endpackage
