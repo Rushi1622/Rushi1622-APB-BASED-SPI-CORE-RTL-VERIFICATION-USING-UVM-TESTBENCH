a3b92b8a1fe2bde74d2b85b93eaecb7a6633f854
Rushikesh Balasaheb Wakde <155816684+Rushi1622@users.noreply.github.com>
2026-05-30 16:37:13 +0530

    Update env_config.sv

--- TB/env_config.sv
+++ TB/env_config.sv
@@ -1,1 +1,19 @@
+class env_config extends uvm_object;
 
+	`uvm_object_utils(env_config)
+		//to control the agents and scoreboard directly
+		bit has_spi_agent =1;
+		bit has_apb_agent =1;
+		bit has_scoreboard =1;
+
+		//both agent config
+		apb_config apb_conh; 
+		spi_config spi_conh; 
+		
+		
+		function new (string name = "env_config");
+			super.new(name);
+		endfunction
+		
+		
+endclass 
