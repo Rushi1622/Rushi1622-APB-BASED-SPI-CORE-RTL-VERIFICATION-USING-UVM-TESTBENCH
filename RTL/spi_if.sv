7e5b332aed8551a340af2d4586b9c879227c99f1
Rushikesh Balasaheb Wakde <155816684+Rushi1622@users.noreply.github.com>
2026-05-30 16:49:30 +0530

    Update spi_if.sv

--- RTL/spi_if.sv
+++ RTL/spi_if.sv
@@ -1,1 +1,25 @@
+interface spi_if (input bit clk);
+	logic SCLK;
+	
+	logic SS;
+	logic  MOSI; //spi communicate serially it transfer data bit by bit
+	logic  MISO;	
+
+	clocking spi_drv_cb @(posedge clk);
+		default input #1 output #1 ;
+		output MISO;
+		input SS,SCLK,MOSI;
+	endclocking
+	
+	
+	clocking spi_mon_cb @(posedge clk);
+		default input #1 output #1 ;
+		input SCLK,SS,MOSI,MISO;
+		endclocking
+	
+	modport SPI_DRV_CB (clocking spi_drv_cb);
+	modport SPI_MON_CB (clocking spi_mon_cb);
+	
+endinterface 
+
 
